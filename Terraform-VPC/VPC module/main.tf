2bf3c1d551b4eb14f556ff5c5f07a95ca4c75ac1
balumopuriDSO <balumopuri004@gmail.com>
2026-06-16 09:55:13 +0000

    peering updated

--- Terraform-VPC/VPC module/main.tf
+++ Terraform-VPC/VPC module/main.tf
@@ -162,30 +162,30 @@ resource "aws_route_table_association" "database" {
 }
 
 
-resource "aws_route" "public_peering" {
-    count = var.is_peering_enabled ? 1 : 0
-    route_table_id = aws_route_table.public.id
-    destination_cidr_block = local.default_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
-}
+# resource "aws_route" "public_peering" {
+#     count = var.is_peering_enabled ? 1 : 0
+#     route_table_id = aws_route_table.public.id
+#     destination_cidr_block = local.default_cidr_block
+#     vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+# }
 
-resource "aws_route" "private_peering" {
-    count = var.is_peering_enabled ? 1 : 0
-    route_table_id = aws_route_table.private.id
-    destination_cidr_block = local.default_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
-}
+# resource "aws_route" "private_peering" {
+#     count = var.is_peering_enabled ? 1 : 0
+#     route_table_id = aws_route_table.private.id
+#     destination_cidr_block = local.default_cidr_block
+#     vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+# }
 
-resource "aws_route" "database_peering" {
-    count = var.is_peering_enabled ? 1 : 0
-    route_table_id = aws_route_table.database.id
-    destination_cidr_block = local.default_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
-} 
+# resource "aws_route" "database_peering" {
+#     count = var.is_peering_enabled ? 1 : 0
+#     route_table_id = aws_route_table.database.id
+#     destination_cidr_block = local.default_cidr_block
+#     vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+# } 
 
-resource "aws_route" "default_peering" {
-    count = var.is_peering_enabled ? 1 : 0
-    route_table_id = data.aws_route_table.main.route_table_id
-    destination_cidr_block = var.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
-} 
+# resource "aws_route" "default_peering" {
+#     count = var.is_peering_enabled ? 1 : 0
+#     route_table_id = data.aws_route_table.main.route_table_id
+#     destination_cidr_block = var.cidr_block
+#     vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+# } 
--- Terraform-VPC/VPC module/vpc-peering.tf
+++ Terraform-VPC/VPC module/vpc-peering.tf
@@ -14,3 +14,38 @@ resource "aws_vpc_peering_connection" "main" {
     )
   
 }
+
+# resource "aws_route" "public_perring" {
+#     count = var.is_peering_enabled ? 1 : 0
+#     route_table_id              = aws_route_table.public.id
+#     vpc_peering_connection_id   = aws_vpc_peering_connection.default[count.index].id
+#     destination_cidr_block      = local.default.vpc_cidr
+# }
+
+resource "aws_route" "public_peering" {
+    count = var.is_peering_enabled ? 1 : 0
+    route_table_id = aws_route_table.public.id
+    destination_cidr_block = local.default_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+}
+
+resource "aws_route" "private_peering" {
+    count = var.is_peering_enabled ? 1 : 0
+    route_table_id = aws_route_table.private.id
+    destination_cidr_block = local.default_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+    count = var.is_peering_enabled ? 1 : 0
+    route_table_id = aws_route_table.database.id
+    destination_cidr_block = local.default_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+} 
+
+resource "aws_route" "default_peering" {
+    count = var.is_peering_enabled ? 1 : 0
+    route_table_id = data.aws_route_table.main.route_table_id
+    destination_cidr_block = var.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
+} 
